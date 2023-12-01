996aa31617a7f5ebf77e423faf4f765ae0689057
kwabenahcode <mrofori507@gmail.com>
2023-12-01 08:28:32 +0000

    Changes made on resource screen

--- screens/Resources.js
+++ screens/Resources.js
@@ -1,30 +1,46 @@
 import * as React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 import { Video, ResizeMode } from 'expo-av';
 import { LinearGradient } from 'expo-linear-gradient';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function App() {
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({});
-  return (
-    <LinearGradient colors={['#02080e','#1e1e1e','#a59d9e']} style={styles.container}>
-    <View style={styles.subContainer}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-        }}
-        useNativeControls
-        resizeMode={ResizeMode.CONTAIN}
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
-      />
 
-        <View style={styles.buttons}>
-          
+  const handleDownload = () => {
+    // Implement your download logic here
+    alert('Download PDF');
+  };
+
+  return (
+    <LinearGradient colors={['#02080e', '#1e1e1e', '#a59d9e']} style={styles.container}>
+      <View>
+        <Video
+          ref={video}
+          style={styles.video}
+          source={{
+            uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+          }}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+          isLooping
+          onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+        />
+
+        <View style={styles.textContainer}>
+          <Text style={{ color: '#fff', fontSize: 19, marginTop: -99 }}>
+            {'Download Resources on \n BSc. Computer Science'}
+          </Text>
         </View>
-        
+
+        <View style={styles.downloadContainer}>
+          <Image style={styles.pdfImage} source={require('../assets/favicon.png')} />
+          <TouchableOpacity style={styles.downloadBtn} onPress={handleDownload}>
+            {/* <FontAwesome name="download" size={1} color="#fff" /> */}
+            <Text>Download</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </LinearGradient>
   );
@@ -37,20 +53,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#ecf0f1',
   },
 
-  subContainer:{
-    padding: 10,
-    width: 200
-  },
-
   video: {
     width: 400,
     height: 300,
     marginTop: -390,
   },
 
-  buttons: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+  downloadContainer: {
+    flexDirection: "row",
+    position: "relative",
     alignItems: 'center',
+    marginBottom: -20,
+    marginTop: 90,
+  },
+
+  pdfImage: {
+    width: 40,
+    height: 40,
+    margin: 10,
+  },
+
+  downloadBtn: {
+    position: "absolute",
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 5,
+    margin: 50,
+    marginLeft: 300
+  },
+
+  textContainer: {
+    position: 'absolute',
+    textAlign: 'center',
+    marginLeft: 80,
   },
 });
