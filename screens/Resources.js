ec0751778d5a4a1c6758686ed707403db51661d7
kwabenahcode <mrofori507@gmail.com>
2023-12-01 00:39:02 +0000

    developing the resource screen

--- screens/Resources.js
+++ screens/Resources.js
@@ -1,25 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { ResizeMode } from 'expo-av'
-import VideoPlayer from 'expo-video-player'
+import * as React from 'react';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
 import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Resources() {
+export default function App() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
-    <LinearGradient colors={['#02080e','#1e1e1e','#a59d9e']}>
-        <View>
-            <VideoPlayer
-                videoProps={{
-                    shouldPlay: true,
-                    resizeMode: ResizeMode.CONTAIN,
-                    // ❗ source is required https://docs.expo.io/versions/latest/sdk/video/#props
-                    source: {
-                    uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-                    },
-                }}
-            />
-        <Text>Resources</Text>
+    <LinearGradient colors={['#02080e','#1e1e1e','#a59d9e']} style={styles.container}>
+    <View style={styles.subContainer}>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+
+        <View style={styles.buttons}>
+          
         </View>
+        
+      </View>
     </LinearGradient>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+
+  subContainer:{
+    padding: 10,
+    width: 200
+  },
+
+  video: {
+    width: 400,
+    height: 300,
+    marginTop: -390,
+  },
+
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
