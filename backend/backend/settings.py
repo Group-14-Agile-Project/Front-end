79a26e9b82bc2e8405e26f67d7febf40ab3b3fd7
kwabenahcode <mrofori507@gmail.com>
2024-01-15 16:41:13 +0000

    requirements changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     # 'corsheaders.middleware.CorsMiddleware',
 
 ]
@@ -134,15 +136,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
-MEDIA_ROOT = (BASE_DIR/"media/")
+# STATIC_URL = '/static/'
+# # STATICFILES_DIRS = [
+# #     BASE_DIR / "static",
+# # ]
+# MEDIA_ROOT = (BASE_DIR/"media/")
 
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 
-STATIC_ROOT = (BASE_DIR/"static/")
+# STATIC_ROOT = (BASE_DIR/"static/")
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = "static/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
