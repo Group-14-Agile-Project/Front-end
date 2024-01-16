c338c39e8657b32c16a744508a34d450c890ecac
kwabenahcode <mrofori507@gmail.com>
2024-01-16 12:55:14 +0000

    fix

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("auth/login/", views.SignInView.as_view()),
-    path("auth/sign_up/", views.SignUpView.as_view({'post':'post'})),
+    path("auth/sign_up/", views.SignUpView.as_view()),
     path("auth/resources/", views.ResourcesView.as_view()),
     path("auth/profile/", views.UserProfileView.as_view()),
 
